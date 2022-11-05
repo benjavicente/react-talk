63d9c7d57b8873fb70c976ca0406dea87f645b07
Benjamín Vicente <benjavicente@uc.cl>
2022-11-04 22:56:26 -0300

    chore: add link to repo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Link from "next/link";
 
-import { ArrowRightIcon } from "@heroicons/react/20/solid";
+import { ArrowRightIcon, CodeBracketIcon } from "@heroicons/react/20/solid";
 
 import ReactLogo from "@/components/ReactLogo";
 import { ThemeSwitcher } from "@/lib/theme";
@@ -19,11 +19,15 @@ export default function Home() {
 					Taller de React
 				</h1>
 				<div className="flex flex-wrap gap-2">
+					<ThemeSwitcher />
 					<Link href="/slides/01-intro/" className="btn flex gap-1">
 						Ir a las diapositivas
 						<ArrowRightIcon className="h-5 w-5" />
 					</Link>
-					<ThemeSwitcher />
+					<Link href="https://github.com/benjavicente/react-talk" className="btn flex gap-1">
+						Código fuente
+						<CodeBracketIcon className="h-5 w-5" />
+					</Link>
 				</div>
 				<ReactLogo className="absolute bottom-5 right-2 -z-10 h-96 animate-[spin_8s_linear_infinite] fill-primary stroke-primary opacity-30" />
 			</main>
